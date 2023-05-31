2b2e0bcd2f484745e8619fba2dde09990d14ddae
Ian Lucas <ian.lucas@gmail.com>
2023-05-31 11:55:48 +0200

    Adding map to smart contract

--- incrementer/lib.rs
+++ incrementer/lib.rs
@@ -1,51 +1,73 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Exmaple:
+/// Example:
 /// #[ink::contract]
-/// mod my_contract {
-///     use super::*;
-/// 
+/// mod mycontract {
+///     use ink::storage::Mapping;
+///
 ///     #[ink(storage)]
 ///     pub struct MyContract {
-///         my_bool: bool,
-///         my_number: u32,
-/// 
-///         my_account: Option<ink::primitives::AccountId>,
-///         my_balance: Option<Balance>,
+///         owner: ink::primitives::AccountId,
+///
+///         /// Store a mapping from AccountIds to a u32
+///         my_map: Mapping<ink::primitives::AccountId, u32>,
 ///     }
-/// 
+///
 ///     impl MyContract {
 ///         #[ink(constructor)]
-///         pub fn new() -> Self {
-///             MyContract {
-///                 my_bool: false,
-///                 my_number: 0,
-///                 my_account: None,
-///                 my_balance: None,
+///         pub fn new(count: u32) -> Self {
+///             let mut my_map = Mapping::default();
+///
+///             let caller = Self::env().caller();
+///             my_map.insert(&caller, &count);
+///
+///             Self {
+///                 owner: Self::env().caller(),
+///                 my_map,
 ///             }
 ///         }
-/// 
+///
+///         /// Get the number associated with the caller's AccountId, if it exists
 ///         #[ink(message)]
-///         pub fn get(&self) {
-///             /// Contract Message
+///         pub fn get(&self) -> u32 {
+///             let caller = Self::env().caller();
+///             self.my_map.get(&caller).unwrap_or_default()
+///         }
+///     }
+///
+///     #[cfg(test)]
+///     mod tests {
+///         use super::*;
+///
+///         #[ink::test]
+///         fn constructor_works() {
+///             let my_contract = MyContract::new(1);
+///             assert_eq!(1, my_contract.get());
 ///         }
 ///     }
 /// }
 #[ink::contract]
 mod incrementer {
+    use ink::storage::Mapping;
 
     #[ink(storage)]
     pub struct Incrementer {
         // Storage Declaration
         value: i32,
+        my_map: Mapping<ink::primitives::AccountId, i32>,
     }
 
     impl Incrementer {
         #[ink(constructor)]
         pub fn new(init_value: i32) -> Self {
+            let mut new_map = Mapping::default();
+            let caller = Self::env().caller();
+            new_map.insert(caller, &0);
+
             // Contract Constructor
             Incrementer {
                 value: init_value,
+                my_map: new_map,
             }
         }
 
@@ -53,6 +75,7 @@ mod incrementer {
         pub fn default() -> Self {
             Incrementer {
                 value: Default::default(),
+                my_map: Default::default(),
             }
         }
 
@@ -64,6 +87,26 @@ mod incrementer {
         #[ink(message)]
         pub fn inc(&mut self, by:i32) {
             self.value += by
+        }
+
+        #[ink(message)]
+        pub fn get_mine(&self) -> i32 {
+            let caller: ink::primitives::AccountId = Self::env().caller();
+
+            self.my_map.get(caller).unwrap_or_default()
+        }
+
+        #[ink(message)]
+        pub fn set_mine(&mut self, new_val: i32) {
+            let caller = Self::env().caller();
+            self.my_map.insert(caller, &new_val);
+        }
+
+        #[ink(message)]
+        pub fn remove_mine(&self) {
+            let caller = Self::env().caller();
+
+            self.my_map.remove(caller);
         }
 
     }
@@ -96,6 +139,33 @@ mod incrementer {
             assert_eq!(-9, inc1.get());
         }
 
+        #[ink::test]
+        fn get_mine_works() {
+            let contract = Incrementer::new(10);
+
+            assert_eq!(10, contract.get());
+            assert_eq!(0, contract.get_mine());
+        }
+
+        #[ink::test]
+        fn set_mine_works() {
+            let mut contract = Incrementer::default();
+            assert_eq!(0, contract.get_mine());
+
+            let new_val = 10;
+            contract.set_mine(new_val);
+            assert_eq!(new_val, contract.get_mine());
+        }
+
+        #[ink::test]
+        fn remove_mine_works() {
+            let mut contract = Incrementer::default();
+
+            contract.set_mine(10);
+
+            contract.remove_mine();
+            assert_eq!(0, contract.get_mine());
+        }
+
     }
 }
-
