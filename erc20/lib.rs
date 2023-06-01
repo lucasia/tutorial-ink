a7ce24f306f4e987c3c8a6752fb04ef0218e5129
Ian Lucas <ian.lucas@gmail.com>
2023-06-01 15:56:07 +0200

    Adding balance to erc20 example

--- erc20/lib.rs
+++ erc20/lib.rs
@@ -2,68 +2,86 @@
 
 #[ink::contract]
 mod erc20 {
+    use ink::storage::Mapping;
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    /// Create storage for a simple ERC-20 contract.
     #[ink(storage)]
     pub struct Erc20 {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        /// Total token supply.
+        total_supply: Balance,
+        /// Mapping from owner to number of owned tokens.
+        balances: Mapping<ink::primitives::AccountId, Balance>,
     }
 
     impl Erc20 {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
+        /// Create a new ERC-20 contract with an initial supply.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(total_supply: Balance) -> Self {
+            let mut balances = Mapping::default();
+            let caller = Self::env().caller();
+
+            balances.insert(caller, &total_supply);
+
+            Self {
+                total_supply,
+                balances,
+            }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
+        /// Returns the total token supply.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn total_supply(&self) -> Balance {
+            self.total_supply
         }
 
-        /// Simply returns the current value of our `bool`.
+        /// Returns the account balance for the specified `owner`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn balance_of(&self, owner: ink::primitives::AccountId) -> Balance {
+            self.balances.get(owner).unwrap_or_default()
         }
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
     #[cfg(test)]
     mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
 
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let erc20 = Erc20::default();
-            assert_eq!(erc20.get(), false);
+        // We define some helper Accounts to make our tests more readable
+        fn default_accounts() -> ink::env::test::DefaultAccounts<Environment> {
+            ink::env::test::default_accounts::<Environment>()
         }
 
-        /// We test a simple use case of our contract.
+        fn alice() -> ink::primitives::AccountId {
+            default_accounts().alice
+        }
+
+        fn bob() -> ink::primitives::AccountId {
+            default_accounts().bob
+        }
+
         #[ink::test]
-        fn it_works() {
-            let mut erc20 = Erc20::new(false);
-            assert_eq!(erc20.get(), false);
-            erc20.flip();
-            assert_eq!(erc20.get(), true);
+        fn new_works() {
+            let contract = Erc20::new(777);
+            assert_eq!(contract.total_supply(), 777);
+        }
+
+        #[ink::test]
+        fn balance_works() {
+            let orig_caller = ::ink::env::caller::<Environment>();
+            assert_eq!(orig_caller, alice()); // alice is the original caller
+
+            let contract = Erc20::new(100);
+            assert_eq!(contract.total_supply(), 100);
+            assert_eq!(contract.balance_of(alice()), 100);
+            assert_eq!(contract.balance_of(bob()), 0);
+        }
+
+        #[ink::test]
+        fn change_caller() {
+            let orig_caller = ::ink::env::caller::<Environment>();
+            assert_eq!(orig_caller, alice()); // alice is the original caller
+
+            ink::env::test::set_caller::<Environment>(bob()); // change our caller to bob
+            assert_eq!(::ink::env::caller::<Environment>(), bob()); // verify
         }
     }
 }
